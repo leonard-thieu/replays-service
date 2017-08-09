58648bc4f15c74b1caa098afac5f11d498f72f32
Leonard Thieu <leonard.thieu@gmail.com>
2017-08-09 14:58:13 -0400

    Migrate files from toofz-necrodancer.

---
+++ toofz.NecroDancer.Leaderboards.ReplaysService/ReplaySettings.cs
@@ -0,0 +1,9 @@
+﻿using toofz.NecroDancer.Leaderboards.Services.Common;
+
+namespace toofz.NecroDancer.Leaderboards.ReplaysService
+{
+    sealed class ReplaySettings : Settings
+    {
+        public int ReplaysPerUpdate { get; set; }
+    }
+}
